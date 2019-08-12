f4efad99776c6018bd6554ede02c0b266c15e9d3
Dubovskoi, Roman <roman.dubovskoi@kronshtadt.ru>
2019-08-12 14:30:08 +0300

    added modules

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,10 +11,6 @@ variable public_key_path {
   description = "Path to the public key used for ssh access"
 }
 
-variable disk_image {
-  description = "Disk image"
-}
-
 variable app_disk_image {
   description = "Disk image for reddit app"
   default     = "reddit-app-1565557162"
